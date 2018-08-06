ee5b0f76afab0ed84d207671c8f5bea89a8b7d22
dean <dean@phoenixnet.com.tw>
2018-08-06 14:26:14 +0800

    init

--- AppCenterTest/AppDelegate.swift
+++ AppCenterTest/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("ccca77e8-1dce-4959-acac-2b79a7d39795", withServices:[ MSAnalytics.self, MSCrashes.self ])
         return true
     }
 
--- AppCenterTest/ViewController.swift
+++ AppCenterTest/ViewController.swift
@@ -13,13 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAlertWithCompletion(title: "hihi", message: "ooo") {
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func showAlertWithCompletion(title: String, message: String, complention: @escaping () -> Void) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(
+            title: "Yes",
+            style: .default,
+            handler: {
+                (_: UIAlertAction!) -> Void in
+                DispatchQueue.main.async {
+                    complention()
+                }
+        }
+        )
+        alertController.addAction(okAction)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
 
 }
 
